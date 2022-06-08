5e17fe4cf6cf66c28fcafb8131109b5733d83187
colson raphael <raphael.kasese@gmail.com>
2022-06-08 18:51:15 +0200

    jeux nombre magique

---
+++ nombreMagique/nombreMagique/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace nbMagique
+{
+    class Program
+    {
+        static void NombreMagique()
+        {
+            const int nbMagique = 8;
+            int nombreVie = 5;
+            int reponseUserInt = 0;
+            
+            while (reponseUserInt != nbMagique)
+            {
+                Console.WriteLine("Deviner un nombre entre (1 et 10)");
+                string reponseUserStr = Console.ReadLine();
+
+                try
+                {
+                    reponseUserInt = int.Parse(reponseUserStr);
+
+                    if(reponseUserInt < nbMagique)
+                    {
+                        nombreVie--;
+                        Console.WriteLine($"Le nombre magique est plus grand il vous reste {nombreVie}");
+                    }
+                    else if (reponseUserInt > nbMagique)
+                    {
+                        nombreVie--;
+                        Console.WriteLine($"Le nombre magique est plus petit il vous reste {nombreVie}");
+                    }
+                    else
+                    {
+                        Console.WriteLine
+                        ($"Bravo vous avez trouver \nLe nombre magique était bien le {reponseUserInt}"); ;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Vous devez rentrer un nombre valide");
+                }
+
+                 if (nombreVie == 0)
+                 {
+                    Console.WriteLine($"Vous avez perdu le nombre magique était {nbMagique}");
+                    break;
+
+                 }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            NombreMagique();
+        }
+    }
+}
+

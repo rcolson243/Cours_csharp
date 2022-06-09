a3dd1eba9b3cb5f51cca286715d2eef0b0a42edd
colson raphael <raphael.kasese@gmail.com>
2022-06-09 17:19:32 +0200

    nouveau exo

---
+++ jeuxDeMaths/jeuxDeMaths/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void JeuxDeMaths()
+        {
+            Console.Write("5 + 4 = ");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}

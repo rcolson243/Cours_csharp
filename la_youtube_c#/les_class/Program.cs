f841d946992fe449706fa1e229699c5c3ea4537e
colson raphael <raphael.kasese@gmail.com>
2022-06-30 13:45:25 +0200

    hello

---
+++ la_youtube_c#/les_class/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class les_class
+    {
+        
+        public struct Tel
+        {
+            public string marque;
+            public int prix;
+
+        }
+        static void Main(string[] args)
+        {
+            Tel a ;
+            a.marque = "Iphone";
+            a.prix = 1000;
+            System.Console.WriteLine(a.prix);
+
+        }
+    }
+}

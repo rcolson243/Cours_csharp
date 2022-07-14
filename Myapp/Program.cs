eee516dee05d69d81c0e4f016f9a817e35d68a87
colson raphael <raphael.kasese@gmail.com>
2022-07-14 16:19:03 +0200

    mise à jour

---
+++ Myapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static void demande_nombre() {
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- premier_programme11/premier_programme/table.cs
+++
@@ -1,5 +0,0 @@
-int[] table = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-for (int i = 0; i < table.Length; i++) {
-    System.Console.WriteLine(i);
-}
